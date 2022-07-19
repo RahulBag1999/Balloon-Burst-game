b227c1310be06955c541381d25bce0a62193b8ac
Rahul Bag <rahulbag.2070@gmail.com>
2022-07-19 17:45:01 +0530

    update

--- Balloon Burst game/Assets/pinMovement.cs
+++ Balloon Burst game/Assets/pinMovement.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class pinMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private Transform pinTriggerPoint;
+    [SerializeField] private Transform pinRespawnPoint;
     private bool move = false;
    
     void Update()
     {
-        if (Input.GetMouseButton(1))
+        if(Input.GetMouseButton(0))
         {
             move = true;
         }
         if(move==true)
         {
         transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+        if(this.transform.position.y >= pinTriggerPoint.position.y)
+        {
+            move = false;
+            this.transform.position= pinRespawnPoint.position;
         }
     }
 }
